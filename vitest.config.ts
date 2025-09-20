17fa7637652a0a972564f7ee2ee7cbfbfb6ccb89
Eric Mika <eric@ericmika.com>
2025-09-19 21:40:48 -0400

    Add test coverage.

---
+++ vitest.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+	test: {
+		coverage: {
+			exclude: ['src/**/*.d.ts', 'src/**/types.ts', 'src/**/*.test.ts', 'src/**/*.spec.ts'],
+			include: ['src/**/*.ts'],
+			provider: 'v8',
+			reporter: ['text', 'html', 'lcov'],
+			reportsDirectory: './coverage',
+			thresholds: {
+				branches: 80,
+				functions: 80,
+				lines: 80,
+				statements: 80,
+			},
+		},
+		pool: 'forks',
+		poolOptions: {
+			forks: {
+				singleFork: true,
+			},
+		},
+	},
+})
